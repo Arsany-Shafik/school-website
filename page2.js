de95bde891a0d3b631a89c38bd9f2c9a84bc0502
arsany shafik <arsany.shafik.fci.scu@gmail.com>
2024-12-02 18:47:46 +0200

    first commit

---
+++ page2.js
@@ -0,0 +1,8 @@
+// function validateForm() {
+//     let x = document.forms["myForm"]["fname"].name;
+//     if (x == "") {
+//       alert("First name must be filled out");
+//       return false;
+//     }
+//     return( true );
+//   }
